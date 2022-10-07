8452386e797c80e8baf2f86c876a3530b308ac37
mohammedashrafhabib01 <mando1mando111@gmail.com>
2022-10-07 20:45:45 +0200

    Update FakeDataSource.kt

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,15 +6,10 @@ import com.udacity.project4.locationreminders.data.dto.Result
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
     private  var remindersData = mutableListOf<ReminderDTO>()
-    private var databaseError = false
-    fun databaseError(boolean: Boolean){
-        databaseError=boolean
-    }
+
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         try {
-            if (databaseError){
-                return Result.Error("There is an error in database")
-            }
+
             return Result.Success(remindersData.toList())
         }catch (ex:Exception){
             return Result.Error(ex.message)
@@ -26,10 +21,7 @@ class FakeDataSource : ReminderDataSource {
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         try {
-            val ans=remindersData.find { it.id==id }
-            if(ans==null||databaseError){
-                return Result.Error("Data is not found")
-            }
+            val ans= remindersData.find { it.id==id } ?: return Result.Error("Data is not found")
             return Result.Success(ans)
         }catch (ex:Exception){
             return Result.Error(ex.message)
